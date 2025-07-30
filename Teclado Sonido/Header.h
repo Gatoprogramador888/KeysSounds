fc0d728c6c8208ddb7ef0dc896d0852f0be38c4b
Gatoprogramador888 <143905112+Gatoprogramador888@users.noreply.github.com>
2025-07-30 12:47:18 -0600

    Agregar archivos de proyecto.

---
+++ Teclado Sonido/Header.h
@@ -0,0 +1,7 @@
+#pragma once
+#pragma once
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+#include <string.h>
+#include <string>
---
+++ Teclado Sonido/resource1.h
@@ -0,0 +1,21 @@
+//{{NO_DEPENDENCIES}}
+// Archivo de inclusión generado de Microsoft Visual C++.
+// Usado por KeySoundEditor.rc
+//
+#define IDD_DIALOG1                     101
+#define BTN_ADD                         1001
+#define BTN_ADD2                        1002
+#define BTN_ADD3                        1003
+#define IDC_EDIT1                       1004
+#define CB_LIST                         1005
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1006
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
